b4c26ac965658c83498a2c0f17711c6296dd2ade
Gaurav yadav <gauravnyadav6@gmail.com>
2024-02-24 21:17:02 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <Toaster position="bottom-right" reverseOrder={false} />
         {children}
+        <Analytics />
       </body>
     </html>
   );
